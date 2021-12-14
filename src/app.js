bb362df9f5c5e55f25cf8fc4589fb3ef999d93f8
jingshiunming <85504934+jingshiunming@users.noreply.github.com>
2021-12-14 15:24:00 +0100

    changes css

---
+++ src/app.js
@@ -0,0 +1,5 @@
+let apiKey = "76349913cec595a868a9f5f72e1b5db8";
+let apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?q=London&units=metric";
+
+axios.get(`${apiUrl}&appid=${apiUrl}`).then();
