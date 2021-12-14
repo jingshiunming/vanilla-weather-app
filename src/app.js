0039bc7f65541ceae659980a445cbfa6e01e7daf
jingshiunming <85504934+jingshiunming@users.noreply.github.com>
2021-12-14 17:21:26 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -28,6 +28,7 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElemente = document.querySelector("#date");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
