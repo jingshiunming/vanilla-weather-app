17ec77caa76073d2afdfc37c41547feecc66e72c
jingshiunming <85504934+jingshiunming@users.noreply.github.com>
2021-12-15 15:04:59 +0100

    added the search engine

--- src/app.js
+++ src/app.js
@@ -43,8 +43,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "76349913cec595a868a9f5f72e1b5db8";
-let city = "Milan";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "76349913cec595a868a9f5f72e1b5db8";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("Milan");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
