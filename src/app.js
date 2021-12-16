ae937ed7fe654e029afa0dc9aab8f536ac1ac893
jingshiunming <85504934+jingshiunming@users.noreply.github.com>
2021-12-16 10:15:10 +0100

    minor changes

--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@ function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
   if (hours < 10) {
-    hours = `0&{hours}`;
+    hours = `0${hours}`;
   }
   let minutes = date.getMinutes();
   if (minutes < 10) {
-    mintues = `0&{mintues}`;
+    mintues = `0${mintues}`;
   }
   let days = [
     "Sunday",
