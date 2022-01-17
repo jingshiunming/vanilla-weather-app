7a5726805cde56f567563e780ab5441e180ac6d2
jingshiunming <85504934+jingshiunming@users.noreply.github.com>
2022-01-17 16:48:27 +0100

    add HTML to javascript

--- src/app.js
+++ src/app.js
@@ -21,6 +21,32 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thur", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+      <img 
+        src="http://openweathermap.org/img/wn/50d@2x.png" 
+        alt="" 
+        width="60">
+      <div class="weather-forecast-temperature">
+       <span class="weather-forecsat-temperature-max"> 18° </span>
+       <span class="weather-forecast-temperature-min"> 12° </span>
+      </div>
+    </div>
+      `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -82,3 +108,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Milan");
+displayForecast();
